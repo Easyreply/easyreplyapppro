f5ed63101434d4fd32f806d2e47fa7b534cefa4f
Easyreply <easyreply.superai@gmail.com>
2025-05-17 23:15:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -113,7 +113,7 @@ def generate_reply(review_text, tone="Professional", reply_length="Medium",
             {"role": "user", "content": prompt}
         ],
         temperature=0.7,
-        max_tokens=700
+        max_tokens=400
     )
 
     return response['choices'][0]['message']['content'].strip()
